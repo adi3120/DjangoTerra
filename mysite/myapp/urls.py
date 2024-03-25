90b11800028588a7f74a8a0fd45197721c2578d4
adi3120 <aditya.yadav.cs.20@ggits.net>
2024-03-26 01:04:44 +0530

    added files

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+	 path('aws/', views.aws, name='aws'),
+	 path('azure/', views.azure, name='azure'),
+	 path('gcp/', views.gcp, name='gcp'),
+	 path('vmware/', views.vmware, name='vmware'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'myapp/home.html')
+
+def aws(request):
+    return render(request,"myapp/aws.html")
+
+def azure(request):
+    return render(request,"myapp/azure.html")
+	
+def gcp(request):
+    return render(request,"myapp/gcp.html")
+
+def vmware(request):
+    return render(request,"myapp/vmware.html")
